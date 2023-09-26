5c67c6721647ac004e1a63a43b79f4b4cd75df28
Fiza Khan <fiza1892003@gmail.com>
2023-09-27 01:00:54 +0530

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🎉', '🥳', '✨', '🎊', '🍰'],
+    }).then(() => jsConfetti.addConfetti())
+})
